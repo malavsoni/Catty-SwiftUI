ca082eb436986885d57af977fb2b4906f56c84be
Malav Soni <11372923+malavsoni@users.noreply.github.com>
2022-07-03 11:02:18 +0530

    CI-CD Implemented (#2)
    Github Actions Added

--- Catty/CattyTests/Tests/CatsUseCaseTests.swift
+++ Catty/CattyTests/Tests/CatsUseCaseTests.swift
@@ -66,12 +66,4 @@ class CatsUseCaseTests: XCTestCase {
             }
         }
     }
-    
-    func testPerformanceExample() throws {
-            // This is an example of a performance test case.
-        self.measure {
-                // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
